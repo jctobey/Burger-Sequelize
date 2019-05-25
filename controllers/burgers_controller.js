f42193fd03aec004db76c2006ede3b4b021a9171
James Tobey <jtobey@ssicorp.socialsolutions.com>
2019-05-24 21:45:37 -0500

    everything workin except location.reload...somethin to look at!

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,60 +1,54 @@
-var express=require("express");
-var burger=require("../models/burger.js")
+var express = require("express");
+var db = require("../models");
 
 var router = express.Router();
 
-
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  db.burger.findAll({}).then(function(hbsObject) {
     var hbsObject = {
-      burgers: data
+      burgers: hbsObject
     };
-   
+
     res.render("index", hbsObject);
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+  db.burger.create(req.body).then(function(dbfound_pets) {
+    res.json(dbfound_pets);
   });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+  var condition = req.params.id;
 
   console.log("condition", condition);
 
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+  db.burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    {
+      where: {
+        id: condition
+      }
     }
-  });
+  );
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  burger.deleteOne(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   burger.deleteOne(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,38 @@
-var orm=require("../config/orm.js");
-
-var burger = {
-    selectAll: function(cb) {
-      orm.selectAll("burgers", function(res) {
-        cb(res);
-      });
+module.exports = function(sequelize, Sequelize) {
+  var burger = sequelize.define("burger", {
+    burger_name: {
+      type: Sequelize.STRING
     },
-    // The variables cols and vals are arrays.
-    insertOne: function(cols, vals, cb) {
-      orm.insertOne("burgers", cols, vals, function(res) {
-        cb(res);
-      });
-    },
-    updateOne: function(objColVals, condition, cb) {
-      orm.updateOne("burgers", objColVals, condition, function(res) {
-        cb(res);
-      });
-    },
-    deleteOne: function(condition, cb) {
-      orm.deleteOne("burgers", condition, function(res) {
-        cb(res);
-      });
+    devoured: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
     }
-  };
-  
-  module.exports=burger;
+  });
+  return burger;
+};
+
+// var burger = {
+//     selectAll: function(cb) {
+//       orm.selectAll("burgers", function(res) {
+//         cb(res);
+//       });
+//     },
+//     // The variables cols and vals are arrays.
+//     insertOne: function(cols, vals, cb) {
+//       orm.insertOne("burgers", cols, vals, function(res) {
+//         cb(res);
+//       });
+//     },
+//     updateOne: function(objColVals, condition, cb) {
+//       orm.updateOne("burgers", objColVals, condition, function(res) {
+//         cb(res);
+//       });
+//     },
+//     deleteOne: function(condition, cb) {
+//       orm.deleteOne("burgers", condition, function(res) {
+//         cb(res);
+//       });
+//     }
+//   };
+
+//   module.exports=burger;
